5d89e3df48edd67330a891375d5dac494cbe012d
Kevin Trabajo <kevin.alvarenga93@gmail.com>
2023-07-13 11:34:31 -0600

    clase03 completada

--- 5. Manejo del DOM/script.js
+++ 5. Manejo del DOM/script.js
@@ -1,9 +1,25 @@
-const boton = document.querySelector('[data-form-btn]');
+const btn = document.querySelector('[data-form-btn]');
+
 const createTask = (evento) => {
     evento.preventDefault();
-    const getInput = document.querySelector('[data-form-input]');
-    console.log(getInput.value);
-}   
+    const input = document.querySelector('[data-form-input]');
+    const value = input.value;
+    const list = document.querySelector('[data-list]');
+    const task = document.createElement('li');
+    task.classList.add('card');
+    input.value = '';
+    const content = `<div>
+        <i class="far fa-check-square icon"></i>
+        <span class="task">${value}</span>
+        </div>
+        <i class="fas fa-trash-alt transIcon icon"></i>`;
+    console.log(content);
+    
+    list.appendChild(task);
+    
+    task.innerHTML = content;
 
+};
 
-boton.addEventListener('click', createTask);
+console.log(btn);
+btn.addEventListener('click', createTask);
